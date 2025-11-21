e3392a62a705303f0d7843591fa448749a461d63
Byn Byn <rya199232@gmail.com>
2025-11-21 17:59:47 +0700

    day3 homepage - story + service +  why choose us  + experience  + process + banner + sơ luoc

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
-// src/pages/HomePage.tsx
 import { Header } from "../components/layout/Header";
+import { HomeBanner } from "../features/home/HomeBanner"; // <--- IMPORT
+import { HomeExperience } from "../features/home/HomeExperience";
+import { HomeProcess } from "../features/home/HomeProcess";
+import { HomeServices } from "../features/home/HomeServices";
+import { HomeStory } from "../features/home/HomeStory";
+import { HomeWelcome } from "../features/home/HomeWelcome";
+import { HomeWhyChoose } from "../features/home/HomeWhyChoose";
 
 export const HomePage = () => {
   return (
@@ -7,10 +13,32 @@ export const HomePage = () => {
       className="relative bg-white"
       style={{ width: "1440px", minHeight: "100vh" }}
     >
+      {/* Header luôn nằm trên cùng */}
       <Header />
 
-      <div className="h-screen flex items-center justify-center text-3xl font-bold">
-        Trang Chủ
+      {/* Banner chính của trang chủ */}
+      <HomeBanner />
+
+      <div style={{ marginTop: "39px" }}>
+        <HomeWelcome />
+      </div>
+      <div style={{ marginTop: "41px" }}>
+        <HomeStory />
+      </div>
+
+      <div style={{ marginTop: "68px" }}>
+        <HomeServices />
+      </div>
+
+      <div style={{ marginTop: "140px" }}>
+        <HomeWhyChoose />
+      </div>
+
+      <div style={{ marginTop: "100px" }}>
+        <HomeExperience />
+      </div>
+      <div>
+        <HomeProcess />
       </div>
     </div>
   );
